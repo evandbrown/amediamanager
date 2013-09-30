6020d49a3e271115dfcee91d80e147fceedcb1ec
Evan Brown <evandbrown@gmail.com>
2013-09-29 23:28:01 -0700

    Update build tools to publish WAR to buckets in us-east-1 and us-west-2, and CFN templates to support deploy from both buckets

--- run-cfn/push-war.sh
+++ run-cfn/push-war.sh
@@ -2,5 +2,12 @@
 cd ..
 mvn clean
 mvn package
-aws s3 cp --acl public-read --region us-east-1 target/amediamanager.war s3://evbrown/public/
+
+# Upload war to bucket in multiple regions
+regions=(us-east-1 us-west-2)
+for var in "${regions[@]}"
+do
+	echo "Uploading to amm-${var}"
+  aws s3 cp --acl public-read --region ${var} target/amediamanager.war s3://amm-${var}/public/
+done
 cd run-cfn
