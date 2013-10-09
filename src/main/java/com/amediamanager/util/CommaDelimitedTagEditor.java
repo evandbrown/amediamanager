28d4c5ad0438654fbe52d61a94a84ce789bca380
Evan Brown <evandbrown@gmail.com>
2013-10-08 20:04:03 -0700

    Replace TagSet implementation with Set interface

--- src/main/java/com/amediamanager/util/CommaDelimitedTagEditor.java
+++ src/main/java/com/amediamanager/util/CommaDelimitedTagEditor.java
@@ -1,19 +1,31 @@
 package com.amediamanager.util;
 
 import java.beans.PropertyEditorSupport;
+import java.util.HashSet;
 import java.util.Set;
 
-import com.amediamanager.domain.TagSet;
+import org.apache.commons.lang.StringUtils;
 
 public class CommaDelimitedTagEditor extends PropertyEditorSupport {
 
+	// Convert a string to a Set of strings
     public void setAsText(String text) {
-        Set<String> tags = new TagSet<String>();
+        Set<String> tags = new HashSet<String>();
         String[] strings = text.split(", ");
         for(int i =0; i < strings.length; i++) {
             String tag = strings[i];
             tags.add(tag);
         }
         setValue(tags);
+    }
+    
+    // Convert a tag list to a comma-delimited string
+    @SuppressWarnings("unchecked")
+	public String getAsText() {
+    	String concatenated = new String();
+    	if(null != getValue()) {
+    		concatenated = StringUtils.join((Set<String>)getValue(), ", ");
+    	}
+    	return concatenated;
     }
 }
--- src/main/java/com/amediamanager/util/PrivacyEditor.java
+++ src/main/java/com/amediamanager/util/PrivacyEditor.java
@@ -1,10 +1,8 @@
 package com.amediamanager.util;
 
 import java.beans.PropertyEditorSupport;
-import java.util.Set;
 
 import com.amediamanager.domain.Privacy;
-import com.amediamanager.domain.TagSet;
 
 public class PrivacyEditor extends PropertyEditorSupport {
 
