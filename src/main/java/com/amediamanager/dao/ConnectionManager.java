3dac4ba1fcdd2f23b1ce3591a755ba71b1e94676
Evan Brown <evandbrown@gmail.com>
2013-10-13 12:06:47 -0700

    Reuse Connection from hibernate rather than creating one manually for db schema checks

--- src/main/java/com/amediamanager/dao/ConnectionManager.java
+++
@@ -1,8 +0,0 @@
-package com.amediamanager.dao;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionManager {
-    public Connection getConnection() throws SQLException;
-}
