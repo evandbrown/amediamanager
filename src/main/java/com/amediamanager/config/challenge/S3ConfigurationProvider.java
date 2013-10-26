dcbe7e333ef3a00cfc889ad1e32ef20c785da4c9
Evan Brown <evandbrown@gmail.com>
2013-10-26 12:21:30 -0700

    Update config challenge and course manual

--- src/main/java/com/amediamanager/config/challenge/S3ConfigurationProvider.java
+++ src/main/java/com/amediamanager/config/challenge/S3ConfigurationProvider.java
@@ -1,10 +1,15 @@
 package com.amediamanager.config.challenge;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class S3ConfigurationProvider extends com.amediamanager.config.S3ConfigurationProvider {
+	private static final Logger LOG = LoggerFactory.getLogger(S3ConfigurationProvider.class);
+	
 	@Override
 	public void loadProperties() {
 		/**
-		 * - Use super.getBucket() and super.getKey() for the S3 bucke and key.
+		 * - Use super.getBucket() and super.getKey() for the S3 bucket and key.
 		 * - AmazonS3Client does not need credentials
 		 * - Call super.setProperties with your result. 
 		 */
