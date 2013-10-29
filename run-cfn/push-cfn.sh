11a29806376c94d83fa6eb8c716ec2bc4e31efc5
Evan Brown <evandbrown@gmail.com>
2013-10-29 15:41:49 -0700

    Update cfn staging scripts

--- run-cfn/push-cfn.sh
+++ run-cfn/push-cfn.sh
@@ -5,5 +5,6 @@ regions=(us-east-1 us-west-2)
 for var in "${regions[@]}"
 do
 	echo "Uploading to amm-${var}"
-  aws s3 cp --acl public-read --region ${var} launch-app.json s3://amm-${var}/public/
+  aws s3 cp --acl public-read --region ${var} amm-master.json s3://amm-${var}/public/
+  aws s3 cp --acl public-read --region ${var} amm-app-env.json s3://amm-${var}/public/
 done
